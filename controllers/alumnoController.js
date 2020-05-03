2d58377d463238e4c44ce14722d399cbcc2aee8d
Micaela Schejner Aquino <micaela.schejner@eset.com>
2020-05-03 15:36:52 -0300

    Mejoras en mensajes

--- controllers/alumnoController.js
+++ controllers/alumnoController.js
@@ -5,18 +5,23 @@ const fs = require('fs');
 exports.getAlumnos = (req, res) => {
     const alumnos = JSON.parse(fs.readFileSync('./datos/alumnosDatos.json'));
     if (_.isEmpty(req.query)) {
-        res.status(200).json(alumnos);
+        res.status(200).json({
+            "alumnos" : alumnos 
+          });
     } else {
         if(req.query.edadMin && req.query.edadMax){
             var resRango = alumnos.map(alumnoRango => {
                 if(alumnoRango.edad >= req.query.edadMin && alumnoRango.edad <= req.query.edadMax)
                     return alumnoRango
             }).filter(alumnoRango => !_.isEmpty(alumnoRango));
-            res.status(200).json(resRango);
+            res.status(200).json({
+                "cantidad" : Object.keys(resRango).length,
+                "alumnos" : resRango 
+              });
         }else{
             var error = {};
             error.mensaje = 'Parámetros incorrectos';
-            res.status(400).json(error);
+            res.status(400).json({"mensaje" : error});
         }
         
     }
@@ -29,9 +34,11 @@ exports.getAlumnosPorDni = (req, res,next) => {
         if (!alumno) {
             var error = {};
             error.mensaje = 'Alumno no encontrado';
-            res.status(404).json(error);
+            res.status(404).json({"mensaje" : error});
         }else{
-            res.status(200).json(alumno);
+            res.status(200).json({
+                "alumno" : alumno 
+              });
         }     
 
     }catch(e){
@@ -49,7 +56,7 @@ exports.agregarAlumnos = async (req, res, next) => {
         if (alumno) {
             var error = {};
             error.mensaje = 'Alumno ya existe';
-            res.status(403).json(error);
+            res.status(403).json({"mensaje" : error});
         }else{
             if (req.body.id && req.body.nombre && req.body.apellido && req.body.dni && req.body.edad) {
                 const nuevoAlumno = {
@@ -61,12 +68,15 @@ exports.agregarAlumnos = async (req, res, next) => {
                   };
                   alumnos.push(nuevoAlumno);
                   fs.writeFileSync('./datos/alumnosDatos.json', JSON.stringify(alumnos));
-                  res.status(201).json(nuevoAlumno);
+                  res.status(201).json({
+                      "mensaje" : "Alumno creado correctamente",
+                      "alumno" : nuevoAlumno 
+                    });
                 
             }else{
                 var error = {};
                 error.mensaje = 'Request incorrecto';
-                res.status(400).json(error);
+                res.status(400).json({"mensaje" : error});
             }
             
         }
@@ -84,10 +94,10 @@ exports.modificarAlumnoPorId = (req, res,next) => {
         if (!alumno) {
             var error = {};
             error.mensaje = 'Alumno no encontrado';
-            res.status(404).json(error);
+            res.status(404).json({"mensaje" : error});
         }else{
             const nuevaDataAlumno = {
-                id: req.body.id,
+                id: alumno.id,
                 nombre: req.body.nombre,
                 apellido: req.body.apellido,
                 dni: req.body.dni,
@@ -102,7 +112,10 @@ exports.modificarAlumnoPorId = (req, res,next) => {
               });
       
               fs.writeFileSync('./datos/alumnosDatos.json', JSON.stringify(modificacion));
-              res.status(201).json(nuevaDataAlumno);
+              res.status(201).json({
+                    "mensaje": "Alumno actualizado correctamente",
+                    "alumno" : nuevaDataAlumno
+                });
         }
         
       } catch (e) {
@@ -129,7 +142,7 @@ exports.deleteAlumnosPorId = (req, res, next) => {
                 }
             }).filter(alumnoEliminado => !_.isEmpty(alumnoEliminado));
             fs.writeFileSync('./datos/alumnosDatos.json', JSON.stringify(eliminacion));
-            res.status(200).json('Alumno eliminado correctamente');
+            res.status(200).json({ "mensaje": 'Alumno eliminado correctamente'});
         }
         
       } catch (e) {
